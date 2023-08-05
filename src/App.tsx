6b638720b92595223044f60fe49547b2c8ff5793
Gabrielle Guimarães de Oliveira <gabrielle1guim@gmail.com>
2023-08-05 12:38:30 +0000

    feat: add simple line based text editor

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,93 @@
-import {useState} from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
 import './App.css';
+import {FiPlay, FiZap} from 'react-icons/fi';
+import {useEffect, useRef, useState} from 'react';
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [text, setText] = useState('');
+  const containerRef = useRef<HTMLDivElement>(null);
+  const linesRef = useRef<HTMLDivElement>(null);
+  const textRef = useRef<HTMLTextAreaElement>(null);
+
+  useEffect(() => {
+    const text = textRef.current!;
+
+    const updateLines = () => {
+      const container = containerRef.current!;
+      const lines = linesRef.current!;
+      const text = textRef.current!;
+
+      const lineCount = text.value.split('\n').length;
+      const lineNumbers = Array.from(
+        {length: container.clientHeight / 22},
+        () => '~',
+      );
+      for (let i = 0; i < lineCount; i++) {
+        lineNumbers[i] = `${i + 1}`;
+      }
+
+      lines.innerHTML = lineNumbers.join('<br>');
+    };
+
+    updateLines();
+
+    text.addEventListener('input', updateLines);
+
+    return () => {
+      text.removeEventListener('input', updateLines);
+    };
+  }, []);
 
   return (
-    <>
-      <div>
-        <a href='https://vitejs.dev' target='_blank'>
-          <img src={viteLogo} className='logo' alt='Vite logo' />
-        </a>
-        <a href='https://react.dev' target='_blank'>
-          <img src={reactLogo} className='logo react' alt='React logo' />
-        </a>
+    <div className='container'>
+      <div className='wrapper'>
+        <div className='code-editor'>
+          <header>
+            <h1>Theorem Prover</h1>
+
+            <div className='buttons'>
+              <button aria-label='Run code'>
+                <FiPlay />
+              </button>
+              <button aria-label='Type check code'>
+                <FiZap />
+              </button>
+            </div>
+          </header>
+
+          <main ref={containerRef}>
+            <div ref={linesRef as never} className='lines'></div>
+
+            <textarea
+              ref={textRef}
+              onChange={(event) => setText(event.target.value)}
+              onKeyDown={(event) => {
+                if (event.key == 'Tab') {
+                  event.preventDefault();
+
+                  const current = textRef.current!;
+                  const start = current.selectionStart;
+                  const end = current.selectionEnd;
+
+                  // set textarea value to: text before caret + tab + text after caret
+                  current.value =
+                    current.value.substring(0, start) +
+                    '  ' +
+                    current.value.substring(end);
+
+                  // put caret at right position again
+                  current.selectionStart = current.selectionEnd = start + 2;
+                }
+              }}
+              name='Code'
+              cols={30}
+              rows={10}
+            >
+              {text}
+            </textarea>
+          </main>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className='card'>
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className='read-the-docs'>
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </div>
   );
 }
 
